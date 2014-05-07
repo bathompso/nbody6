c863c40510a0eaa9a915b2cf03ce814d85e785a2
bathompso <bathompso@gmail.com>
2014-05-07 15:22:10 -0500

    Added MCluster source and documentation

--- compile.sh
+++ compile.sh
@@ -1,4 +1,9 @@
+# Make NBODY6
 cd src
 make
 rm *.o
 mv nbody6 ../
+
+# Make MCluster
+gcc -O2 -fopenmp -ffast-math -I /usr/local/Cellar/gcc47/4.7.3/lib/gcc/x86_64-apple-darwin13.1.0/4.7.3/include/ -o mcluster main.c -lm
+mv mcluster ../
